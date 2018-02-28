65b534dafed7e2ffc3a68e3cabf1169a45ba11ea
zuoshifan <zuoshifan@163.com>
2018-02-27 22:46:35 -0500

    Changes data preparition files

--- prepare_img.py
+++ prepare_img.py
@@ -5,38 +5,39 @@ import glob
 import scipy.misc as misc
 
 
-keyword = 'bathroom'
-# keyword = 'bedroom'
-src_dir = './downloads/%s/' % keyword
+src_dir = './downloads/'
 dest_dir = './data/'
 
 train_size = 1600
-validation_size = 352
+validation_size = 320
 img_width, img_height = 150, 150
 
-files = glob.glob(src_dir+'/*.jpg')
-# print len(files)
-# print path.basename(files[0]), path.basename(files[0]).split('.')[0]
-# print path.basename(files[1]), path.basename(files[1]).split('.')[0]
+keywords = ['bathroom', 'bedroom', 'livingroom', 'kitchen']
 
-for fi, fl in enumerate(files):
-    fl_name = path.basename(fl)
-    idx, suffix = fl_name.split('.')
-    if fi < train_size:
-        new_name = 'train/%s/%s%04d.%s' % (keyword, keyword, fi+1, suffix)
-    elif fi < train_size + validation_size:
-        new_name = 'validation/%s/%s%04d.%s' % (keyword, keyword, fi-train_size+1, suffix)
-    else:
-        break
-    new_fl_name = dest_dir+new_name
-    new_fl_path = path.dirname(new_fl_name)
-    if not path.isdir(new_fl_path):
-        os.makedirs(new_fl_path)
-
-    # copy file to new dir
-    # shutil.copyfile(fl, new_fl_name)
-
-    # resize image and save to file
-    img = misc.imread(fl)
-    new_img = misc.imresize(img, [img_width, img_height], interp='nearest')
-    misc.imsave(new_fl_name, new_img)
+for keyword in keywords:
+    files = glob.glob(src_dir+'%s/*.jpg' % keyword)
+    # print len(files)
+    # print path.basename(files[0]), path.basename(files[0]).split('.')[0]
+    # print path.basename(files[1]), path.basename(files[1]).split('.')[0]
+    
+    for fi, fl in enumerate(files):
+        fl_name = path.basename(fl)
+        idx, suffix = fl_name.split('.')
+        if fi < train_size:
+            new_name = 'train/%s/%s%04d.%s' % (keyword, keyword, fi+1, suffix)
+        elif fi < train_size + validation_size:
+            new_name = 'validation/%s/%s%04d.%s' % (keyword, keyword, fi-train_size+1, suffix)
+        else:
+            break
+        new_fl_name = dest_dir+new_name
+        new_fl_path = path.dirname(new_fl_name)
+        if not path.isdir(new_fl_path):
+            os.makedirs(new_fl_path)
+    
+        # copy file to new dir
+        # shutil.copyfile(fl, new_fl_name)
+    
+        # resize image and save to file
+        img = misc.imread(fl)
+        new_img = misc.imresize(img, [img_width, img_height], interp='nearest')
+        misc.imsave(new_fl_name, new_img)
--- sample_numbers.py
+++ sample_numbers.py
@@ -1,8 +1,9 @@
 import glob
 
 
-print 'train bedroom:', len(glob.glob('./data/train/bedroom/*.jpg'))
-print 'train bathroom:', len(glob.glob('./data/train/bathroom/*.jpg'))
-print 'validation bedroom:', len(glob.glob('./data/validation/bedroom/*.jpg'))
-print 'validation bathroom:', len(glob.glob('./data/validation/bathroom/*.jpg'))
+types = ['train', 'validation']
+keywords = ['bathroom', 'bedroom', 'livingroom', 'kitchen']
 
+for tp in types:
+    for kw in keywords:
+        print '%s %s:' % (tp, kw), len(glob.glob('./data/%s/%s/*.jpg' % (tp, kw)))
